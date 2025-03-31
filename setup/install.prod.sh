3bada2f04e428a9deb51b09ad5e8dde0cb4562d5
dekopon21020014 <110777220+dekopon21020014@users.noreply.github.com>
2025-04-01 00:45:32 +0900

    fix: excelとcsvのバージョンを同時にインストールするように変更

--- setup/install.prod.sh
+++ setup/install.prod.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# for excel version
 docker load -i ./anonymize-excel.tar
 cp anonymize-excel.desktop ~/デスクトップ/
+
+# for csv version
 docker load -i ./anonymize-csv.tar
 cp anonymize-csv.desktop ~/デスクトップ/
