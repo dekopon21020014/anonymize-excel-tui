233b23000810d3e7f04bf641a2630a7a17d5545c
dekopon21020014 <110777220+dekopon21020014@users.noreply.github.com>
2025-04-01 00:42:21 +0900

    fix

--- setup/build.prod.sh
+++ setup/build.prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker build --platform=linux/amd64 -f ./excel.dockerfile -t anonymize-excel:amd64 ../src
 docker build --platform=linux/amd64 -f ./Dockerfile -t anonymize-csv:amd64 ../src
-#docker save -o anonymize-excel.tar anonymize-excel:amd64
 docker save -o anonymize-csv.tar anonymize-csv:amd64
+#docker build --platform=linux/amd64 -f ./excel.dockerfile -t anonymize-excel:amd64 ../src
+#docker save -o anonymize-excel.tar anonymize-excel:amd64
